1695eab94fbf267f612a966138f342233932ea1b
Maaz Amir <amirmaaz16@gmail.com>
2025-10-07 16:48:19 +0530

    feat: enhance onboarding flow with role selection and navigation

--- hireWeb/src/components/protected-route.jsx
+++ hireWeb/src/components/protected-route.jsx
@@ -11,7 +11,9 @@ const ProtectedRoute = ({ children }) => {
         return <Navigate to={`/?sign-in=true`} />
     }
 
-    //check onboarding status
+    if (user !== undefined && !user.unsafeMetadata?.role && pathname !== '/onboarding') {
+        return <Navigate to={`/onboarding`} />
+    };
     return children;
 }
 
