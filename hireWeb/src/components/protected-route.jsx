0a2682f59731f18e6b14f460ffafa7a956d5bbab
Maaz Amir <amirmaaz16@gmail.com>
2025-10-07 15:23:48 +0530

    feat: implement ProtectedRoute component for route protection and update routes in App.jsx

---
+++ hireWeb/src/components/protected-route.jsx
@@ -0,0 +1,18 @@
+import { useUser } from '@clerk/clerk-react'
+import React from 'react'
+import { Navigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+    const { isSignedIn, user, isLoaded } = useUser();
+    const { pathname } = useLocation();
+
+    if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+        return <Navigate to={`/?sign-in=true`} />
+    }
+
+    //check onboarding status
+    return children;
+}
+
+export default ProtectedRoute
